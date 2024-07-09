092d5fe818d0a09d4b262e67fb9289cd258ab111
atul-cvc <atulk@tekskillsinc.net>
2024-07-09 16:42:42 +0530

    commit for adding user controller views

--- QPR_Application/Controllers/UserController.cs
+++ QPR_Application/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using QPR_Application.Models.Entities;
 using QPR_Application.Repository;
 
@@ -23,11 +24,74 @@ namespace QPR_Application.Controllers
             return View(allUsers);
         }
 
+        public IActionResult CreateUser()
+        {
+            //Populate the dropdown options
+            var loginTypes = new List<SelectListItem>{
+                //new SelectListItem { Value = "null", Text = "Select" },
+                new SelectListItem { Value = "ROLE_CVO", Text = "ROLE_CVO" },
+                new SelectListItem { Value = "ROLE_COORD", Text = "ROLE_COORD" },
+                new SelectListItem { Value = "ROLE_ADMIN", Text = "ROLE_ADMIN" }
+            };
+            ViewBag.LoginTypes = new SelectList(loginTypes, "Value", "Text", "Select");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUser(registration registerUser)
+        {
+            try
+            {
+                _userRepo.CreateUser(registerUser);
+                return RedirectToAction("AllUsers");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CreateExceptionMsg = "User not created \n" + ex.Message.ToString();
+                return View();
+            }
+        }
+
         // GET: UserController/Details/5
         public async Task<IActionResult> Details(string Id)
         {
             var User = await _userRepo.GetUserDetails(Id);
             return View(User);
         }
+
+        public async Task<IActionResult> Edit(string Id)
+        {
+            var User = await _userRepo.GetUserDetails(Id);
+
+            //Populate the dropdown options
+            var loginTypes = new List<SelectListItem>{
+                //new SelectListItem { Value = "null", Text = "Select" },
+                new SelectListItem { Value = "ROLE_CVO", Text = "ROLE_CVO" },
+                new SelectListItem { Value = "ROLE_COORD", Text = "ROLE_COORD" },
+                new SelectListItem { Value = "ROLE_ADMIN", Text = "ROLE_ADMIN" }
+            };
+            ViewBag.LoginTypes = new SelectList(loginTypes, "Value", "Text", User.logintype);
+
+            return View(User);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(registration User)
+        {
+            try
+            {
+                _userRepo.EditUser(User);
+                return RedirectToAction("Details", "User", new { id = User.usercode.ToString() });
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CreateExceptionMsg = "User edit failed \n" + ex.Message.ToString();
+                return View();
+            }
+        }
+
     }
 }
--- QPR_Application/Repository/IUserRepo.cs
+++ QPR_Application/Repository/IUserRepo.cs
@@ -1,4 +1,5 @@
-﻿using QPR_Application.Models.Entities;
+﻿using NuGet.RuntimeModel;
+using QPR_Application.Models.Entities;
 
 namespace QPR_Application.Repository
 {
@@ -6,5 +7,7 @@ namespace QPR_Application.Repository
     {
         public Task<IEnumerable<registration>> GetAllUsers();
         public Task<registration> GetUserDetails(string Id);
+        public Boolean CreateUser(registration registration);
+        public void EditUser(registration registration);
     }
 }
