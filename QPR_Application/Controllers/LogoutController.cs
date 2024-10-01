4ba5da4b75cb4927301d1dc602b007599ba191e8
atul-cvc <atulk@tekskillsinc.net>
2024-10-01 15:46:23 +0530

    commit for Logout Successfull page

--- QPR_Application/Controllers/LogoutController.cs
+++ QPR_Application/Controllers/LogoutController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using QPR_Application.Repository;
 using Microsoft.EntityFrameworkCore;
 using QPR_Application.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace QPR_Application.Controllers
-{
+{   
     public class LogoutController : Controller
     {
         private readonly ILogger<LogoutController> _logger;
@@ -15,12 +15,23 @@ namespace QPR_Application.Controllers
         public LogoutController(ILogger<LogoutController> logger, IHttpContextAccessor httpContext, QPRContext dbContext)
         {
             _httpContext = httpContext;
-            //_logger = logger;
+            _logger = logger;
             _dbContext = dbContext;
         }
+
+        [Authorize]
         public async Task<IActionResult> Index()
         {
+            //string _user = _httpContext.HttpContext.Session.GetString("UserName");
+            //"UserName"
+            //"ipAddress"
+            //"UserRole"
+            //orgcode
             _httpContext.HttpContext.Session.Remove("CurrentUser");
+            _httpContext.HttpContext.Session.Remove("UserName");
+            _httpContext.HttpContext.Session.Remove("ipAddress");
+            _httpContext.HttpContext.Session.Remove("UserRole");
+            _httpContext.HttpContext.Session.Remove("orgcode");
             _httpContext.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             foreach (var cookie in HttpContext.Request.Cookies.Keys)
             {
@@ -29,11 +40,17 @@ namespace QPR_Application.Controllers
             }
             await _dbContext.Database.CloseConnectionAsync();
             var connection = _dbContext.Database.GetDbConnection();
-            if(connection.State == System.Data.ConnectionState.Open)
+            if (connection.State == System.Data.ConnectionState.Open)
             {
                 await connection.CloseAsync();
             }
-            return RedirectToAction("Index", "Login");
+            _logger.LogInformation("User logged out successfully.");
+            return RedirectToAction("LogoutSuccessfull");
+        }
+
+        public IActionResult LogoutSuccessfull()
+        {
+            return View();
         }
     }
 }
